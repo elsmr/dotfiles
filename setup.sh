8d72413d4d34f24ed1f19c2e85a82901a4bf9b2d
Elias Meire <me@eliasmei.re>
2017-10-24 10:19:08 +0200

    restructure setup.sh

--- setup.sh
+++ setup.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env bash
 
-#
-#
-#
+function setup {
+    # update to latest dotfiles
+    git pull origin master;
+    
+    # install XCode CLI tools if needed
+    if ! xcode-select -p > /dev/null; then
+        xcode-select --install
+    fi
 
-# update to latest dotfiles
-git pull origin master;
+    # backup home directory dotfiles
+    rsync -a --progress --no-perms --exclude ".git/" --exclude ".DS_Store" ~/.[^.]* ~/home-backup
+    # copy only root dotfiles into home directory
+    rsync -a --progress --no-perms --exclude ".git/" --exclude ".DS_Store" $(pwd)/.[^.]* ~
+
+    # Run setup for all tools (comment out what you don't need or want)
+    ( cd homebrew && ./setup.sh )
+    ( cd fish && ./setup.sh )
+    ( cd iterm && ./setup.sh )
+    ( cd vscode && ./setup.sh )
+}
 
 # get user confirmation
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/N) " -n 1;
 echo "";
 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    # backup home directory dotfiles
-    rsync -a --progress --no-perms --exclude ".git/" --exclude ".DS_Store" ~/.[^.]* ~/home-backup
-    # copy only root dotfiles into home directory
-    rsync -a --progress --no-perms --exclude ".git/" --exclude ".DS_Store" $(pwd)/.[^.]* ~
+    setup
 fi;
-
-# install XCode CLI tools if needed
-if ! xcode-select -p > /dev/null; then
-    xcode-select --install
-fi
-
-# Run setup for all tools (comment out what you don't need or want)
-( cd homebrew && ./setup.sh )
-( cd fish && ./setup.sh )
-( cd iterm && ./setup.sh )
-( cd vscode && ./setup.sh )
