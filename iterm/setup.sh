56dbf6812d846d35f723da038ca34501844bc63c
Elias Meire <me@eliasmei.re>
2017-10-24 17:48:49 +0200

    iterm config

--- iterm/setup.sh
+++ iterm/setup.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/env bash
+
+# copy prefs into ~/.iterm2
+rsync -a --progress --no-perms --exclude ".git/" --exclude ".DS_Store" ./com.googlecode.iterm2.plist ~/.iterm2
+
+# tell iterm to load prefs from ~/.iterm2
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.iterm2"
