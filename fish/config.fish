2211de7924bae676bf856b00ba233729c7b2b676
Elias Meire <me@eliasmei.re>
2017-11-21 18:08:59 +0100

    add private file and set EDITOR to vim

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,4 @@
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-source ~/.config/fish/aliases.fish
+test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
+test -e {$HOME}/.config/fish/aliases.fish; and source {$HOME}/.config/fish/aliases.fish
+test -e {$HOME}/.config/fish/private.fish; and source {$HOME}/.config/fish/private.fish
+set -Ux EDITOR vim
