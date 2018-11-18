c21643ccd3d60023e4aad76f404c99af432eb076
Elias Meire <me@eliasmei.re>
2018-11-18 17:07:54 +0100

    Add vscode and rust to PATH

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,6 @@
 test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
 test -e {$HOME}/.config/fish/aliases.fish; and source {$HOME}/.config/fish/aliases.fish
 test -e {$HOME}/.config/fish/private.fish; and source {$HOME}/.config/fish/private.fish
-set -Ux EDITOR vim
+
+set EDITOR vim
+set PATH $HOME/.cargo/bin "/Applications/Visual Studio Code.app/Contents/Resources/app/bin" $PATH
