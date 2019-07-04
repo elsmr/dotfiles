ec605bda2c7d94e035eb1ee48cac50e6074d4974
Elias Meire <me@eliasmei.re>
2019-07-04 21:50:45 +0200

    Add more dirs to PATH in fish config

--- fish/config.fish
+++ fish/config.fish
@@ -4,4 +4,6 @@ test -e {$HOME}/.config/fish/private.fish; and source {$HOME}/.config/fish/priva
 
 set -x EDITOR vim
 set -x PATH $HOME/.cargo/bin "/Applications/Visual Studio Code.app/Contents/Resources/app/bin" $PATH
-set -x PATH $HOME/emsdk $HOME/emsdk/clang/e1.38.21_64bit $HOME/emsdk/node/8.9.1_64bit/bin $HOME/emsdk/emscripten/1.38.21 $PATH
+set -x PATH $HOME/emsdk $HOME/emsdk/clang/e1.38.21_64bit $HOME/emsdk/emscripten/1.38.21 $PATH
+
+status --is-interactive; and source (rbenv init -|psub)
